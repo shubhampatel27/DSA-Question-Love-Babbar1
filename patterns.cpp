c12f2e2f718eb97bc5647923df5614131f13ac84
shubhampatel27 <shubhampateldz@gmail.com>
2022-12-29 16:42:04 +0530

    pattern

---
+++ patterns.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< "hi";
+    cin>>n;
+   int i =1;
+    while (i<=n)  
+    {
+        /* code */
+
+     int j = 1;
+       while (j<=n)
+       {
+         cout<<"#";
+         j++;
+       }
+       cout<<endl;
+       i++;
+       
+    }
+    
+}
