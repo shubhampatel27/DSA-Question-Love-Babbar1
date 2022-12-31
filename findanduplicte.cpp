f97f04d57b72eca8aff195b9a2b1ad033c9f8529
shubhampatel27 <shubhampateldz@gmail.com>
2022-12-31 21:51:47 +0530

    31/12/2022

---
+++ findanduplicte.cpp
@@ -0,0 +1,9 @@
+int findDuplicate(vector<int>&arr){
+    int ans=0;
+    for(int i=0; i<arr.size();i++;){
+        ans=ans^arr[i];
+    }
+    for(int i=1; i<arr.size();i++;){
+        ans=ans^i;
+    }
+}
