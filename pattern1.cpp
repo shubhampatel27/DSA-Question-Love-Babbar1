dd3fe45a212d7c3df6d7b6de0a1855e9b2b647d8
shubhampatel27 <shubhampateldz@gmail.com>
2022-12-31 21:38:22 +0530

    12/31/2022

--- pattern1.cpp
+++ pattern1.cpp
@@ -4,16 +4,17 @@ int main(){
     int n;
     cin>> n;
     int row =1;
-   
+  
       while (row<=n)
     {
- int value = row;
+      int value = row;
      int col=1;
     while (col<=row)
      {
-        cout<<value << " ";
-        //cout<<(row-col + 1) << " ";
-        value--;
+        // char ch= 'A'+col-1;
+        // cout<<ch<< " ";
+        cout<<(row - col+1) << " ";
+        value++;
         col++;
      }cout << endl;
       
