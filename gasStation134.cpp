760585c4e6a28c54600577a053f13fb0750879a5
shubhampatel27 <shubhampateldz@gmail.com>
2023-02-06 22:36:43 +0530

    gasStationQuestionUpdate

--- gasStation134.cpp
+++ gasStation134.cpp
@@ -1,22 +1,22 @@
-class Solution {
-public:
-    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int lower_index=0;
-        int totalGas=0;
-        int totalCost=0;
-        int currentCost=0;
-        int n =gas.size(); 
-        for(int i=0; i<n; i++){
-             totalGas +=gas[i];
-             totalCost +=cost[i];
-             currentCost = currentCost + gas[i]-cost[i];
-             if(currentCost<0){
-               lower_index=i+1;
-               currentCost =0;
-             }
-        }if(totalGas>=totalCost)
-          return lower_index;
+// class Solution {
+// public:
+//     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+//         int lower_index=0;
+//         int totalGas=0;
+//         int totalCost=0;
+//         int currentCost=0;
+//         int n =gas.size(); 
+//         for(int i=0; i<n; i++){
+//              totalGas +=gas[i];
+//              totalCost +=cost[i];
+//              currentCost = currentCost + gas[i]-cost[i];
+//              if(currentCost<0){
+//                lower_index=i+1;
+//                currentCost =0;
+//              }
+//         }if(totalGas>=totalCost)
+//           return lower_index;
     
-    return -1;
-    }
-};
+//     return -1;
+//     }
+// };
