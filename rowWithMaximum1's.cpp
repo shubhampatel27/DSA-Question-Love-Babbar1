8397e8828668ac30d84c5baa0466075cff6af9a0
shubhampatel27 <shubhampateldz@gmail.com>
2023-02-06 23:24:17 +0530

    importantQuestion 06/02/2023

---
+++ rowWithMaximum1's.cpp
@@ -0,0 +1,20 @@
+// #include <bits/stdc++.h> 
+// int rowMaxOnes(vector<vector<int>>& mat, int n, int m) {
+//     // Write your code here.
+//     int col = m-1;
+//     int row =0;
+//     for(int i=0; i<n; i++){
+//         for(int j=col; j>=0; j--){
+//             if(mat[i][j]==1){
+//                 row = i;
+//                 col--;
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//     }
+//     return row;
+
+// }
+
