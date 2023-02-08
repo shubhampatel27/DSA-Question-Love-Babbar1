b1729ecce7d88602941025056fbf6a9a2e1eb134
shubhampatel27 <shubhampateldz@gmail.com>
2023-02-08 23:33:41 +0530

    BookAllocationProblem 08/02/2023

---
+++ somePractiseQuestion/bookAllocationProblem.cpp
@@ -0,0 +1,35 @@
+// #include <bits/stdc++.h> 
+// using namespace std;
+// bool isPossible (vector<int> arr, int n, int m ,int mid){
+//     int student =1,pageSum=0;
+//     for(int i=0;i<n;i++)
+//     if(pageSum+arr[i]<=mid){
+//         pageSum  +=arr[i];
+//     } else{
+//         student++;
+//         if(student>m||arr[i]>mid)return false;
+//         pageSum=arr[i];
+
+//     } return true;
+// }
+// int allocateBooks(vector<int> arr, int n, int m) {
+//     // Write your code here. 
+
+//     int s=0;
+//     int sum=0;
+//     int ans=-1;
+//     for(int i=0; i<n;i++){
+//         sum+=arr[i];
+//     }
+//     int e=sum;
+//     int mid = s+(e-s)/2;
+//     while(s<=e){
+//     if(isPossible(arr,n,m,mid)){
+//         ans=mid;
+//         e=mid-1;
+//     } else {
+//       s = mid + 1;
+//     }  mid=s+(e-s)/2;
+//     }
+//     return ans;
+// }
