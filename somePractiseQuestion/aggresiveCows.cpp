2f5fb3125d5c93cc4e306aa74fe9008d05fd4788
shubhampatel27 <shubhampateldz@gmail.com>
2023-02-09 11:55:56 +0530

    Update Library in the code

--- somePractiseQuestion/aggresiveCows.cpp
+++ somePractiseQuestion/aggresiveCows.cpp
@@ -1,36 +1,37 @@
-// #include <iostream>
-// using namespace std;
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
 
-// bool isPossible(vector<int> &stalls, int k,int mid){
-//     int cowCount =1;
-//     int lastPosition=stalls[0];
-//     for(int i=1;i<stalls.size(); i++){
-//         if(stalls[i]-lastPosition>=mid){
-//             cowCount++;
-//             if(cowCount==k)return true;
-//             lastPosition=stalls[i];
-//         }
-//     }
-//     return false;
-// }
-// int aggressiveCows(vector<int> &stalls, int k)
-// {
-//     //    Write your code here.
+bool isPossible(vector<int> &stalls, int k,int mid){
+    int cowCount =1;
+    int lastPosition=stalls[0];
+    for(int i=1;i<stalls.size(); i++){
+        if(stalls[i]-lastPosition>=mid){
+            cowCount++;
+            if(cowCount==k)return true;
+            lastPosition=stalls[i];
+        }
+    }
+    return false;
+}
+int aggressiveCows(vector<int> &stalls, int k)
+{
+    //    Write your code here.
 
-//     sort(stalls.begin(),stalls.end());
-//     int start=1;
-//     int end=stalls[stalls.size()-1]-stalls[0];
-//     int ans=-1;
-//     int mid= start+(end-start)/2;
-//     while(start<=end){
-//         mid= start+(end-start)/2;
-//         if(isPossible(stalls,k,mid)){
-//             ans=mid;
-//             start=mid+1;
-//         }else{
-//             end=mid-1;
-//         }
+    sort(stalls.begin(),stalls.end());
+    int start=1;
+    int end=stalls[stalls.size()-1]-stalls[0];
+    int ans=-1;
+    int mid= start+(end-start)/2;
+    while(start<=end){
+        mid= start+(end-start)/2;
+        if(isPossible(stalls,k,mid)){
+            ans=mid;
+            start=mid+1;
+        }else{
+            end=mid-1;
+        }
 
-//     }
-//     return ans;
-// }
+    }
+    return ans;
+}
