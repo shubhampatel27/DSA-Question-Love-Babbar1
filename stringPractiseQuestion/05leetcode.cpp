17bdc6fb7a063b6c73b7dceeb8c359e7f311413e
shubhampatel27 <shubhampateldz@gmail.com>
2023-02-14 23:54:53 +0530

    14/02/2023

--- stringPractiseQuestion/05leetcode.cpp
+++ stringPractiseQuestion/05leetcode.cpp
@@ -32,3 +32,46 @@ string longestPalinSubstring(string s)
         }
         return longeststr;
 }
+
+
+// optimize approach 
+class Solution {
+    public:
+    int  findingLength(string s, int l, int r, int n){
+          while(l >=0 && r < n){
+              if(s[l]==s[r]) {
+                  l-- , r++;
+              }else{
+                  break;
+              }
+          }
+          int len = r-l-1;
+
+      return len;
+    }
+public:
+    string longestPalindrome(string s) {
+        if(s.length()<=1) return s;
+      int max_length =1;
+        int n = s.length();
+      int st=0;
+      int end=0;
+       for(int i=0; i<n-1; i++){
+            int l=i; int r=i;
+       int len1 = findingLength(s,l,r,n);
+      int len2 = findingLength(s,l,r+1,n);
+           int len = max(len1, len2);
+            
+            if(len> max_length)
+            {
+                max_length = len;
+                st = i-(len-1)/2;
+            }
+          }
+      return s.substr(st, max_length);
+
+      }
+};
+
+
+
