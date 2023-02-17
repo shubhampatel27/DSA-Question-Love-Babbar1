ce3da13816d4d6e7f046ac1e4661fedb6cfd3ae9
shubhampatel27 <shubhampateldz@gmail.com>
2023-02-17 23:53:09 +0530

    17/02/2023

---
+++ LinkedList/206leetcode.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h> 
+using namespace std;
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+void reverse(LinkedListNode<int>* &head,LinkedListNode<int>* &prev,LinkedListNode<int>* &curr){
+
+    
+
+       if(head==NULL){
+
+           return;
+
+       }
+
+        while(curr != NULL){
+
+            LinkedListNode<int>* forward = curr->next;
+
+            curr->next =prev;
+
+            prev=curr;
+
+            curr=forward;
+
+ 
+
+        }
+
+        
+
+    }
+
+ 
+
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head) 
+
+{
+
+    LinkedListNode<int>* prev=NULL;
+
+    LinkedListNode<int>* curr=head;
+
+    reverse(head,prev,curr);
+
+    return prev;
+
+    
+
+}
