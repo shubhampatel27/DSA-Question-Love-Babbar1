7c5ea581b3db0ad44c71bb8e20454655906f26ea
shubhampatel27 <shubhampateldz@gmail.com>
2023-02-21 23:20:18 +0530

    21/02/2023

--- LinkedList/02leetocde.cpp
+++ LinkedList/02leetocde.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
- Definition for singly-linked list.
+//  Definition for singly-linked list.
  struct ListNode {
      int val;
      ListNode *next;
-     ListNode() : val(0), next(nullptr) {}
-     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode() : val(0), next(NULL) {}
+     ListNode(int x) : val(x), next(NULL) {}
       ListNode(int x, ListNode *next) : val(x), next(next) {}
   };
  
@@ -34,3 +34,48 @@ public:
     } return dummy->next;
   }
 };
+
+// little changes in the code
+
+
+    // Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+	        Node *next;
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+        };
+
+
+
+Node *addTwoNumbers(Node *head1, Node *head2)
+{
+    // Write your code here.
+    Node * dummy = new Node (0);
+    Node * temp = dummy;
+    int carry = 0;
+    while(head1 !=NULL || head2 !=NULL || carry !=0){
+        int sum =carry;
+        if(head1 !=NULL){
+            sum += head1->data;
+            head1= head1->next;
+        }
+        if(head2 !=NULL){
+            sum += head2->data;
+            head2 = head2->next;
+        }
+       
+        carry = sum/10;
+        Node* node1 = new Node(sum%10);
+        temp->next = node1;
+        temp = temp->next;
+    }
+    temp->next = NULL;
+    return dummy->next;
+}
