c82017f47110ec524c7231d67d66e1f23e37ac10
shubhampatel27 <shubhampatel0305@gmail.com>
2023-02-26 22:49:43 +0530

    26/02/2023

--- arrayPractiseQuestion/169leetcode.cpp
+++ arrayPractiseQuestion/169leetcode.cpp
@@ -1,3 +1,27 @@
+
+
+// brute force approach
+
+using namespace std;
+int majorityElement(vector<int>& nums) {
+    int majority_count = nums.size() / 2;
+
+    for (int i = 0; i < nums.size(); i++) {
+        int count = 0;
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[j] == nums[i]) {
+                count++;
+            }
+        }
+        if (count > majority_count) {
+            return nums[i];
+        }
+    }
+    // This return statement should never be reached, because it is assumed that
+    // the majority element always exists in the array.
+    return -1;
+}
+
 #include<vector>
 using namespace std;
 class Solution {
@@ -45,4 +69,24 @@ int findMajorityElement(int arr[], int n) {
 	}
 
 	return -1;
+}
+
+
+
+// using map 
+#include<map>
+#include<unordered_map> 
+using namespace std;
+
+int findMajorityElement(int arr[], int n) {
+    map<int, int> freq;
+    for (int i = 0; i < n; i++) {
+        freq[arr[i]]++;
+    }
+    for (auto f : freq) {
+        if (f.second > n / 2) {
+            return f.first;
+        }
+    }
+    return -1;
 }
