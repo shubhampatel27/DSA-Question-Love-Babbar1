acf145d6a860a0adb394a401a8fdbaef3fc3aca2
shubhampatel27 <shubhampatel0305@gmail.com>
2023-03-03 19:24:06 +0530

    03/03/2023

--- BinaryTreePractiseQuestion/105leetcode.cpp
+++ BinaryTreePractiseQuestion/105leetcode.cpp
@@ -10,32 +10,31 @@
  * };
  */
 class Solution {
-
-    TreeNode *solve(vector<int>& inorder, vector<int>& postorder, int &index, int inOrderStart, int inOrderEnd, int n ,map<int,int>& nodeToIndex) {
+public:
+TreeNode *solve(vector<int>& inorder, vector<int>& preorder, int &index, int inOrderStart, int inOrderEnd, int n ,map<int,int>& nodeToIndex) {
         // base case
-        if (index < n || inOrderStart > inOrderEnd) return NULL;
+        if (index >= n || inOrderStart > inOrderEnd) return NULL;
       
-        int element = postorder[index--];
+        int element = preorder[index++];
         TreeNode* root = new TreeNode (element);
         int position = nodeToIndex[element];
-
-         root->right = solve(inorder, postorder, index, position +1 , inOrderEnd, n , nodeToIndex );
-        root->left = solve(inorder, postorder, index, inOrderStart , position-1, n , nodeToIndex );
+        
+         root->left = solve(inorder, preorder, index, inOrderStart , position-1, n , nodeToIndex );
+         root->right = solve(inorder, preorder, index, position +1 , inOrderEnd, n , nodeToIndex );
+        
        
 
         return root;
         
     }
-public:
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int n = inorder.size();
         map<int , int > nodeToIndex ;
         for(int i=0; i<n ; i++){
             nodeToIndex[inorder[i]]= i;
         }
-        int index =n-1; 
-        TreeNode* ans = solve(inorder, postorder, index, 0 , n-1 , n , nodeToIndex );
+        int index =0; 
+        TreeNode* ans = solve(inorder, preorder, index, 0 , n-1 , n , nodeToIndex );
         return ans;
-
     }
 };
