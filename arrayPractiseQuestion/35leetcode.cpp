088314858d32e76373ee1c5ab4a921f841ee28ef
Shubham patel <shubhampateldz@gmail.com>
2023-03-30 20:27:46 +0530

    Create 35leetcode.cpp

---
+++ arrayPractiseQuestion/35leetcode.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+   
+        int ans =0;
+        int start =0,end = nums.size()-1;
+        int mid = start + (end-start)/2;
+
+        while(start<=end){
+
+          if(nums[mid] == target) 
+          return mid;
+
+           else if (nums[mid]<target){
+                 start = mid+1;
+           }
+           else 
+           end= mid-1;
+         
+           
+            mid = start + (end-start)/2;
+        }
+        return start;
+    }
+};
