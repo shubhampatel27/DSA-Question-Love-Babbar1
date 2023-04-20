3d239317139c87609cffdd18654c2c7a5ecb6b74
Shubham patel <shubhampateldz@gmail.com>
2023-04-20 23:21:23 +0530

    Create leetcode13.cpp

---
+++ arrayPractiseQuestion/leetcode13.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int n =s.size();
+        unordered_map<char,int> map ={
+            {'I',1},
+            {'V',5},
+            {'X',10},
+            {'L',50},
+            {'C',100},
+            {'D',500},
+            {'M',1000}, 
+           
+            
+        };
+
+      int sum=map[s.back()];
+      for(int i= n-2;i>=0;i--){
+          if(map[s[i]]<map[s[i+1]])
+          sum -=map[s[i]];
+          else 
+          sum+= map[s[i]];
+
+      }
+      return sum;
+    }
+};
